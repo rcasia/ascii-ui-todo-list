41905d6f54627402bed55adc277fcea933def3f4
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-17 12:46:56 +0200

    feat: first approach

---
+++ lua/ascii-todolist/init.lua
@@ -0,0 +1,56 @@
+local ui = require("ascii-ui")
+local Paragraph = ui.components.Paragraph
+local For = require("ascii-ui.components.for")
+local If = require("ascii-ui.components.if")
+
+local M = {}
+
+M.todos = {}
+
+M.open = function()
+	local App = function()
+		local todos, setTodos = ui.hooks.useState(M.todos)
+
+		return ui.layout(
+			--
+			If({
+				condition = function()
+					return #todos() > 0
+				end,
+				child = For({
+					items = todos,
+					transform = function(item)
+						return {
+							content = item,
+						}
+					end,
+					component = ui.components.Paragraph,
+				}),
+				fallback = Paragraph({
+					content = "No todos available",
+				}),
+			}),
+			ui.components.Button({
+				label = "Add Todo",
+				on_press = function()
+					M.todos[#M.todos + 1] = "Todo " .. #M.todos + 1
+
+					setTodos(M.todos)
+				end,
+			})
+		)
+	end
+	ui.mount(App())
+end
+
+setmetatable(M, {
+	__call = function(_, opts)
+		opts = opts or {}
+		return M
+	end,
+})
+
+-- TODO: remove this
+M.open()
+
+return M
