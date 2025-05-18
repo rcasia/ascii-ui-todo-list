0d8f9f6905230de0286210139d365037abdc40f5
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-18 19:26:51 +0200

    refactor: put the related components together in TodoList

--- lua/ascii-todolist/components/TodoList.lua
+++ lua/ascii-todolist/components/TodoList.lua
@@ -1,20 +1,43 @@
 local ui = require("ascii-ui")
 local For = require("ascii-ui.components.for")
 local TodoItem = require("ascii-todolist.components.TodoItem")
+local Layout = ui.layout
+local Paragraph = ui.components.Paragraph
+local Button = ui.components.Button
+local If = require("ascii-ui.components.if")
 
---- @alias TodoListProps { todos: fun(): string[] }
+local function TodoList()
+	local todos, setTodos = ui.hooks.useState({ "Todo 1" })
 
---- @param props TodoListProps
-local function TodoList(props)
-	return For({
-		items = props.todos,
-		transform = function(item)
-			return {
-				content = item,
-			}
-		end,
-		component = TodoItem,
-	})
+	return Layout(
+		Paragraph({ content = "My Today Tasks" }),
+		--
+		Button({
+			label = "Add Todo",
+			on_press = function()
+				local new_todo = vim.fn.input("new todo: ")
+
+				local _todos = todos()
+				_todos[#_todos + 1] = new_todo
+				setTodos(_todos)
+			end,
+		}),
+
+		If({
+			condition = function()
+				return #todos() > 0
+			end,
+			child = For({
+				items = todos,
+				transform = function(item)
+					return {
+						content = item,
+					}
+				end,
+				component = TodoItem,
+			}),
+		})
+	)
 end
 
 return ui.createComponent("TodoItem", TodoList)
--- lua/ascii-todolist/init.lua
+++ lua/ascii-todolist/init.lua
@@ -1,33 +1,12 @@
 local ui = require("ascii-ui")
 local If = require("ascii-ui.components.if")
-local Button = ui.components.Button
 local TodoList = require("ascii-todolist.components.TodoList")
 
 local M = {}
 
 M.open = function()
 	local App = function()
-		local todos, setTodos = ui.hooks.useState({ "Todo 1" })
-
-		return ui.layout(
-			--
-			If({
-				condition = function()
-					return #todos() > 0
-				end,
-				child = TodoList({ todos = todos }),
-			}),
-			Button({
-				label = "Add Todo",
-				on_press = function()
-					local new_todo = vim.fn.input("new todo: ")
-
-					local _todos = todos()
-					_todos[#_todos + 1] = new_todo
-					setTodos(_todos)
-				end,
-			})
-		)
+		return TodoList()
 	end
 	ui.mount(App())
 end
