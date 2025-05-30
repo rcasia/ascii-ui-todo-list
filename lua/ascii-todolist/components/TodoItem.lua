337ca73ec7cfac61f88e675c15e19c54c636f1ed
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-30 22:46:24 +0200

    feat: updates count of done tasks

--- lua/ascii-todolist/components/TodoItem.lua
+++ lua/ascii-todolist/components/TodoItem.lua
@@ -1,10 +1,25 @@
 local ui = require("ascii-ui")
+local Button = ui.components.Button
+local Row = ui.layout.Row
 
---- @alias TodoItemProps { content: string }
+--- @alias TodoItemProps { content: string, done: boolean, toggle: function }
 
 --- @param props TodoItemProps
 local function TodoItem(props)
-	return ui.components.Paragraph({ content = props.content })
+	return Row(
+		--
+		Button({
+			label = function()
+				return props.done and "[x]" or "[ ]"
+			end,
+			on_press = function()
+				if props.toggle then
+					props.toggle()
+				end
+			end,
+		}),
+		ui.components.Paragraph({ content = props.content })
+	)
 end
 
-return ui.createComponent("TodoItem", TodoItem, { content = "string" })
+return ui.createComponent("TodoItem", TodoItem, { content = "string", done = "boolean", toggle = "function" })
--- lua/ascii-todolist/components/TodoList.lua
+++ lua/ascii-todolist/components/TodoList.lua
@@ -6,13 +6,75 @@ local Paragraph = ui.components.Paragraph
 local Button = ui.components.Button
 local If = ui.components.If
 
+--- @alias Todo { id?: string, done: boolean, description: string }
+
+--- @param initial_todos Todo[]
+--- @return fun(): Todo[] todos
+--- @return fun(new_todo: Todo) add_todo
+--- @return fun(id: string)  toggle_todo
+--- @return fun(): number done_count
+local useTodos = function(initial_todos)
+	local todos, dispatch = ui.hooks.useReducer(function(state, action)
+		if action.type == "add" then
+			local id = #state + 1
+			local new_todo = {
+				id = id,
+				done = action.params.new_todo.done or false,
+				description = action.params.new_todo.description,
+			}
+			return vim.list_extend(state, { new_todo })
+		elseif action.type == "toggle" then
+			local id = action.params.id
+
+			return vim.iter(state)
+				:map(function(todo)
+					if todo.id == id then
+						todo.done = not todo.done
+						return todo
+					else
+						return todo
+					end
+				end)
+				:totable()
+		else
+			return state
+		end
+	end, initial_todos)
+
+	local add_todo = function(new_todo)
+		dispatch({ type = "add", params = { new_todo = new_todo } })
+	end
+
+	local toggle_todo = function(id)
+		dispatch({
+			type = "toggle",
+			params = { id = id },
+		})
+	end
+
+	local done_count = function()
+		return vim.iter(todos())
+			:filter(function(todo)
+				return todo.done
+			end)
+			:fold(0, function(acc, _)
+				return acc + 1
+			end)
+	end
+
+	return todos, add_todo, toggle_todo, done_count
+end
+
 local function TodoList()
-	local todos, setTodos = ui.hooks.useState({ "Todo 1" })
+	local todo_list = {
+		{ id = 1, done = true, description = "Todo 1" },
+	}
+	local todos, add_todo, toggle_todo, done_count = useTodos(todo_list)
 
 	return function()
 		return Column(
 			Paragraph({
-				content = ("My Today Tasks! (%d/%d)"):format(0, #todos()),
+				content = ("My Today Tasks! (%d/%d)"):format(done_count(), #todos()),
 			}),
 			--
 			Button({
@@ -20,9 +82,10 @@ local function TodoList()
 				on_press = function()
 					local new_todo = vim.fn.input("new todo: ")
 
-					local _todos = todos()
-					_todos[#_todos + 1] = new_todo
-					setTodos(_todos)
+					add_todo({
+						done = false,
+						description = new_todo,
+					})
 				end,
 			}),
 
@@ -34,7 +97,11 @@ local function TodoList()
 					items = todos,
 					transform = function(item)
 						return {
-							content = item,
+							content = item.description,
+							done = item.done or false,
+							toggle = function()
+								toggle_todo(item.id)
+							end,
 						}
 					end,
 					component = TodoItem,
